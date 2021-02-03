3489b3d3b353049c46ed33603d338129b944ee98
zhangweiming <1210500550@qq.com>
2021-02-03 17:30:35 +0800

    [window]新建子窗口，同时触发选择图片以及子窗口的参数设置

--- renderer.js
+++ renderer.js
@@ -4,3 +4,24 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const { BrowserWindow } = require('electron').remote
+const path = require('path')
+
+const newWindowBtn = document.getElementById('new-window')
+
+newWindowBtn.addEventListener('click', (event) => {
+    const modalPath = path.join('file://', __dirname, 'sections/window/photo.html')
+    let win = new BrowserWindow({
+        width: 600, height: 400,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    })
+
+    win.on('close', () => { win = null })
+    win.loadURL(modalPath)
+    win.show()
+    // // Open the DevTools.
+    // win.webContents.openDevTools()
+})
